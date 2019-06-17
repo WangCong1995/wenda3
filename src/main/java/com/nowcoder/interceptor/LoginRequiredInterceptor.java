717e96a324948f37eb8d79bec4c7be089b6386fc
WangCong <847074803@qq.com>
2019-06-17 12:16:08 +0800

    6月17日 9:49

--- src/main/java/com/nowcoder/interceptor/LoginRequiredInterceptor.java
+++ src/main/java/com/nowcoder/interceptor/LoginRequiredInterceptor.java
@@ -3,13 +3,17 @@ package com.nowcoder.interceptor;
 import com.nowcoder.dao.LoginTicketDAO;
 import com.nowcoder.dao.UserDAO;
 import com.nowcoder.model.HostHolder;
+import com.nowcoder.model.LoginTicket;
+import com.nowcoder.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 /**
  *  将要访问的页面是强制需要登录的。如果你没登录的话，让你跳转到登录页
